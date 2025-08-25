93bdb86074cf541c80e99b796f6d5bada275f628
SiddharthKrishna1 <krishnasiddharth91@gmail.com>
2025-08-25 18:14:52 +0530

    Adding project structure

---
+++ src/main/java/com/example/demo/GeneralVehicleInsuranceTtp2spjs2537Team4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralVehicleInsuranceTtp2spjs2537Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralVehicleInsuranceTtp2spjs2537Team4Application.class, args);
+	}
+
+}
